16725713ae5e13ae448aad9dcd250568235ff32c
kelseytoczek <kelseytoczek@gmail.com>
2022-09-30 12:49:02 -0400

    Entire weather app project loaded from VS to GitHub
    -index.html
    
    src folder:
    -background.jpg
    -index.js
    -package.json
    -style.css

---
+++ src/index.js
@@ -0,0 +1,101 @@
+let currentTime = new Date();
+let dateElement = document.querySelector("#date");
+
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = currentTime.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dayIndex = currentTime.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let dayNumber = currentTime.getDate();
+
+let monthIndex = currentTime.getMonth();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let year = currentTime.getFullYear();
+
+dateElement.innerHTML = `${days[dayIndex]} ${months[monthIndex]} ${dayNumber}, ${year}<br /> ${hours}:${minutes}`;
+
+///
+
+function displayWeatherCondition(response) {
+  document.querySelector(
+    "#city"
+  ).innerHTML = `<i class="fa-solid fa-location-dot location"></i> ${response.data.name}`;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#min-temp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let searchForm = document.querySelector("#search-button");
+searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("New York");
